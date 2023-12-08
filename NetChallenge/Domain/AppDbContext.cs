d9de032c8629021d761dd3c9692e323ef20b5d51
Christian Maczka <christian@whitecloudwt.com>
2023-12-08 19:24:02 -0300

    Add project files.

---
+++ NetChallenge/Domain/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetChallenge.Domain
+{
+    public class AppDbContext :DbContext
+    {
+        public AppDbContext() :base()
+        {            
+        }
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<Office> Offices { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-J8JKVLG;Initial Catalog=OfficeRental; Persist Security Info = False; Trusted_Connection=yes; ");
+        }
+
+    }
+}
---
+++ NetChallenge/Infrastructure/LocationRepository.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using NetChallenge.Abstractions;
+using NetChallenge.Domain;
+
+namespace NetChallenge.Infrastructure
+{
+    public class LocationRepository : ILocationRepository
+    {
+        public List<Location> Locations { get; set; }
+        public LocationRepository()
+        {
+            Locations = new List<Location>();
+        }
+        public IEnumerable<Location> AsEnumerable()
+        {
+            return this.Locations;
+        }
+
+        public void Add(Location item)
+        { 
+            this.Locations.Add(item);
+        }
+    }
+}
